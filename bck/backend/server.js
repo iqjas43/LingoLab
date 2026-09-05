80655ed12b92c62c8c56f6df2c857f2468bcb11a
junaid <207507352+iqjas43@users.noreply.github.com>
2026-09-05 17:37:13 +0530

    server.js changes

--- bck/backend/server.js
+++ bck/backend/server.js
@@ -65,6 +65,6 @@ const grammarRoutes = require('./routes/grammarRoutes');
 app.use('/api/grammar', grammarRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server started on port ${PORT}`);
 });
